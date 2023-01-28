c3adc55a5f2e0fa3d77867b41bc4b86e279d2b69
yuriy <bipedyuriy@gmail.com>
2023-01-27 15:49:36 -1000

    firstArticle

--- src/components/Article/ArticleList.tsx
+++ src/components/Article/ArticleList.tsx
@@ -12,7 +12,7 @@ const ArticleList = (props: Props) => {
                 justifyContent="center"
                 alignItems="center"
             >
-                <Grid item xs={8} sm={7}>
+                <Grid item xs={12}>
                     <ArticleListItem
                         title="Long Established"
                         desc="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
@@ -21,47 +21,6 @@ const ArticleList = (props: Props) => {
             </Grid>
             <br />
             <br />
-            <Grid
-                container
-                spacing={5}
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="center"
-            >
-                <Grid item xs={12} sm={6} md={4}>
-                    <ArticleListItem
-                        title="Long Established"
-                        desc="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ArticleListItem
-                        title="Long Established"
-                        desc="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ArticleListItem
-                        title="Long Established"
-                        desc="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
-                    />
-                </Grid>
-            </Grid>
-            <br />
-            <br />
-            <Grid
-                container
-                direction="row"
-                justifyContent="center"
-                alignItems="center"
-            >
-                <Grid item xs={8} sm={6}>
-                    <ArticleListItem
-                        title="Long Established"
-                        desc="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that...."
-                    />
-                </Grid>
-            </Grid>
         </>
     )
 }
--- src/components/container/Header/Header.tsx
+++ src/components/container/Header/Header.tsx
@@ -4,6 +4,9 @@ import Typography from '@mui/material/Typography'
 import { Container } from '@mui/material'
 import Menu from '../../Menu/Menu'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search'
+import { styled, alpha } from '@mui/material/styles'
+import InputBase from '@mui/material/InputBase'
 
 type Props = {}
 
@@ -21,6 +24,15 @@ const Header = (props: Props) => {
                         >
                             HotCoffe
                         </Typography>
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
                         <Menu />
                     </Toolbar>
                 </Container>
@@ -28,4 +40,46 @@ const Header = (props: Props) => {
         </>
     )
 }
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.black, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.black, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}))
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'black',
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: '',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}))
+
 export default Header
